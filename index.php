446adec6e22eb8327a1d1f36f9d600e23b330971
KevinStephan <kev2969@live.fr>
2015-11-27 11:30:10 +0100

    Initialisation dossier with bootstrap and css

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head lang="fr">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>service web</title> 
+	<meta name="description" content="#"/>	    
+	<meta name="keywords" content="#">
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">	
+	<!-- <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'> -->
+</head>
+
+<body>
+<p>hello</p>
+</body>
+
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>		
+<script src="js/jquery.easing.1.3.js"></script>
+
+</html>
