8dd525d58077bcec67f1cd2aa840ab55fa50c985
vincep69 <vince.pla@laposte.net>
2015-11-27 11:42:56 +0100

    include

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+
+</footer>
+</body>
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>		
+<script src="js/jquery.easing.1.3.js"></script>
+</html>
--- index.php
+++ index.php
@@ -1,23 +1,7 @@
-<!doctype html>
-<html>
-<head lang="fr">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<title>service web</title> 
-	<meta name="description" content="#"/>	    
-	<meta name="keywords" content="#">
-
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">	
-	<!-- <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'> -->
-</head>
-
-<body>
+<?php 
+	include("header.php");
+?>
 <p>hello</p>
-</body>
-
-<script src="js/jquery-1.10.2.min.js"></script>
-<script src="js/bootstrap.min.js"></script>		
-<script src="js/jquery.easing.1.3.js"></script>
-
-</html>
+<?php
+	include("footer.php");
+?>
