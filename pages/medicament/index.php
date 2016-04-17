910f477c32101fbfaa47e82d920d15eb66ebd9b2
vincep69 <vince.pla@laposte.net>
2016-04-17 15:17:13 +0200

    pages index médocs/praticiens

--- pages/medicament/index.php
+++ pages/medicament/index.php
@@ -1,7 +1,63 @@
 <?php
 include '../../inc/header.php';
 include '../../inc/menu.php';
+$reponse = $db->query('SELECT * FROM ppe_medicament ORDER BY MED_DEPOTLEGAL ASC');
 ?>
+
+<table class="table">
+	<thead>
+		<th>
+			Depot légal
+		</th>
+		<th>
+			Nom
+		</th>
+		<th>
+			Code
+		</th>
+		<th>
+			Composition
+		</th>
+		<th>
+			Effets
+		</th>
+		<th>
+			Contre-indication
+		</th>
+	</thead>
+	<tbody>
+	<?php
+	while ($donnees = $reponse->fetch()) {
+		?>
+		
+			<tr>
+				<td><a href="/ZHEVHUYLFH/pages/medicament/medicament_upd.php?depotlegal=<?php echo $donnees['MED_DEPOTLEGAL'] ?>">
+					<?php echo $donnees['MED_DEPOTLEGAL'] ?></a>
+				</td>
+				<td>
+					<?php echo $donnees['MED_NOMCOMMERCIAL'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['FAM_CODE'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['MED_COMPOSITION'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['MED_EFFETS'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['MED_CONTREINDIC'] ?>
+				</td>
+			</tr>
+		
+		<?php
+		}
+		$reponse->closeCursor();
+		?>
+	</tbody>
+</table>
+
 
 <?php
 include '../../inc/footer.php';
--- pages/praticien/index.php
+++ pages/praticien/index.php
@@ -1,7 +1,74 @@
 <?php
 include '../../inc/header.php';
 include '../../inc/menu.php';
+$reponse = $db->query('SELECT * FROM ppe_praticien ORDER BY PRA_NUM ASC');
 ?>
+
+<table class="table">
+	<thead>
+		<th>
+			Numéro
+		</th>
+		<th>
+			Nom
+		</th>
+		<th>
+			Prénom
+		</th>
+		<th>
+			Adresse
+		</th>
+		<th>
+			Code postal
+		</th>
+		<th>
+			Ville
+		</th>
+		<th>
+			Coefficient notoriété
+		</th>
+		<th>
+			Type code
+		</th>
+	</thead>
+	<tbody>
+	<?php
+	while ($donnees = $reponse->fetch()) {
+		?>
+		
+			<tr>
+				<td><a href="/ZHEVHUYLFH/pages/praticien/praticien_upd.php?numero=<?php echo $donnees['PRA_NUM'] ?>">
+					<?php echo $donnees['PRA_NUM'] ?></a>
+				</td>
+				<td>
+					<?php echo $donnees['PRA_NOM'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['PRA_PRENOM'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['PRA_ADRESSE'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['PRA_CP'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['PRA_VILLE'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['PRA_COEFNOTORIETE'] ?>
+				</td>
+				<td>
+					<?php echo $donnees['TYP_CODE'] ?>
+				</td>
+			</tr>
+		
+		<?php
+		}
+		$reponse->closeCursor();
+		?>
+	</tbody>
+</table>
 
 <?php
 include '../../inc/footer.php';
