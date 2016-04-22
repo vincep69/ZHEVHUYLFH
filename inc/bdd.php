f5c8659a4e7f32f4f1338c2eeb73d20d76cfc1d0
vincep69 <vince.pla@laposte.net>
2016-04-22 15:39:25 +0200

    correction accent

--- inc/bdd.php
+++ inc/bdd.php
@@ -8,7 +8,9 @@
 }*/
 /*-------------------------------------------------------ftp----------------------------------------------------*/
 try{
-		$db=new PDO('mysql:host=localhost; dbname=stephan', 'root', '');
+		$db=new PDO('mysql:host=localhost; dbname=stephan', 'root', '', array(
+			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+			);
 	}
 catch(Exception $e){
 		die('Erreur : '.$e->getMessage());
